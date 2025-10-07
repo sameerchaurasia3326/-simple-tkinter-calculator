1a07720ff639954e9e6d48fdf8ffc598e993ff99
sameerchaurasia3326 <sameerchaurasia3326@gmail.com>
2025-10-08 04:48:00 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Create the Flask web app
+app = Flask(__name__)
+
+# Define a route for the homepage
+@app.route('/')
+def hello():
+    return "Hello, World! Your Python app is running."
+
+# This allows the app to be run by a production server
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
