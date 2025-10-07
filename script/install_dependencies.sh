f5989c1b9cc6814636a1852a233a0e46b16537ce
sameerchaurasia3326 <sameerchaurasia3326@gmail.com>
2025-10-08 05:02:40 +0530

    Delete script directory

--- script/install_dependencies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo apt-get update -y
-sudo apt-get install -y python3-pip
-pip3 install Flask gunicorn
--- script/start_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /var/www/html
-gunicorn --bind 0.0.0.0:80 app:app > /dev/null 2>&1 &
