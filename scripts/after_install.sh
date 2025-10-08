a7be263a531062905d35440075030e7da8f399a8
sameerchaurasia3326 <sameerchaurasia3326@gmail.com>
2025-10-09 04:32:40 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+# Update the server's package list and install pip for Python 3
+sudo apt-get update -y
+sudo apt-get install python3-pip -y
+
+# Navigate to the app directory
 cd /home/ubuntu/myapp
+
+# Install Python dependencies from requirements.txt
 sudo pip3 install -r requirements.txt
