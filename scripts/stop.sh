afdd86eac99a527d7d0b4e271af97181afa86b81
sameerchaurasia3326 <sameerchaurasia3326@gmail.com>
2025-10-09 04:54:09 +0530

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "Stopping app..."
-pkill -f app.py || true
+echo "Stopping any running Gunicorn processes..."
+# The '|| true' part ensures the script doesn't fail if no process is found
+sudo pkill gunicorn || true
